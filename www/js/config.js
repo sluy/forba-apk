0363dc15cce53f797f75acc30a94e935129d7788
Stefan Luy <sluy1283@gmail.com>
2022-06-09 13:33:45 -0400

    Services update

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 /*Kartero App Configuration*/
 
-var krms_driver_config ={	
-	'ApiUrl':"",						
-	'DialogDefaultTitle':"Kartero",    
-	'APIHasKey':"",
-	'debug': false
+var krms_driver_config = {
+  ApiUrl: "http://68.183.121.108/api",
+  DialogDefaultTitle: "Forba",
+  APIHasKey: "sd231o1jw8znmpwdr86l1c8sc5zhbj6g",
+  debug: true,
 };
